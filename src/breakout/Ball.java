e4f242880557f807c6f15569aaae77c230815c91
Chris Berryman <c.p.berryman@btinternet.com>
2015-12-23 17:58:19 +0000

    Ball commit

---
+++ src/breakout/Ball.java
@@ -0,0 +1,73 @@
+package breakout;
+
+import java.awt.geom.Ellipse2D;
+
+/**
+ *
+ * @author ChrisBerryman
+ */
+public class Ball {
+
+    /**
+     * Radius of the ball in pixels
+     */
+    public static final int BALL_RADIUS = 10;
+
+    /**
+     * Instance variables
+     */
+    private Ellipse2D.Double circle;
+    private double ballLocationX;
+    private double ballLocationY;
+
+    /**
+     * Constructs a ball
+     *
+     * @param x the x position of the ball
+     * @param y the y position of the ball
+     */
+    public Ball(int x, int y) {
+        circle = new Ellipse2D.Double(x, y, BALL_RADIUS, BALL_RADIUS);
+    }
+
+    /**
+     * Returns the circle of the ball
+     *
+     * @return the circle
+     */
+    public Ellipse2D getCircle() {
+        return circle;
+    }
+
+    /**
+     * Moves the ball to a given point.
+     *
+     * @param x the x location.
+     * @param y the y location.
+     */
+    public void move(double x, double y) {
+        ballLocationX += x;
+        ballLocationY += y;
+        circle.x = ballLocationX;
+        circle.y = ballLocationY;
+    }
+
+    /**
+     * Returns the x location of the ball
+     *
+     * @return the x location
+     */
+    public double getX() {
+        return ballLocationX;
+    }
+
+    /**
+     * Returns the y location of the ball
+     *
+     * @return the y location
+     */
+    public double getY() {
+        return ballLocationY;
+    }
+
+}
